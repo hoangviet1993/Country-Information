5ff9991afc6768a4065ae55537ef77e519a8783a
Viet Tran Quoc Hoang <hoangviet1993@gmail.com>
2020-04-09 22:48:00 -0700

    Navbar with theme switcher (#2)
    * Add material ui and tslint dep
    
    * Add FrontendMentor favicon, add font link and update title
    
    * Add css reset
    
    * Add Navbar component
    
    * Add types folder for interfaces
    
    * Add theme folder for themes
    
    * Add navbar component to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { 
+  MuiThemeProvider,
+} from '@material-ui/core';
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import logo from './logo.svg';
+import * as themes from './themes';
 
 function App() {
+  const [ themeMode, setThemeMode ] = useState(themes.darkTheme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <MuiThemeProvider theme={themeMode}>
+       <Navbar
+          currentThemeMode={themeMode}
+          setThemeMode={setThemeMode}
+        />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
